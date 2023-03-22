23c9af640fa0d636138886e73d2d718b4a8ae64c
Paul Negoescu <paul.negoescu@scoalainformala.ro>
2023-03-22 18:50:35 +0200

    Initial commit with react / vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1 title="Paul" className="highlight">
+        Paul
+      </h1>
+      <a href="https://scoalainformala.ro" target="_blank">
+        Hello from JSX
+      </a>
+    </div>
+  );
+}
+
+export default App;
